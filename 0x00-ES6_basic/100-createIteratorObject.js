9fcf99cbf8c629eb89e932433d42357eaede07d4
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-09 13:34:42 +0200

    Write 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
-export default function* createIteratorObject(report) {
+export default function createIteratorObject(report) {
   const { allEmployees } = report;
   let employeesList = [];
 
   for (const employees of Object.values(allEmployees)) {
     employeesList = employeesList.concat(employees);
   }
-  yield employeesList;
+  return employeesList;
 }
