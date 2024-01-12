a3e45921fa5e4d1cd8ca6b83dab889b0aa8ee970
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-12 23:25:11 +0200

    Finish task_2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,60 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+  getCoffeeBreak() {
+    return 'Getting a coffee break';
+  }
+  workDirectorTasks() {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+  getCoffeeBreak() {
+    return 'Cannot have a coffee break';
+  }
+  workTeacherTasks() {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (employee instanceof Director) {
+    employee.workDirectorTasks();
+  } else if (employee instanceof Teacher) {
+    employee.workTeacherTasks();
+  }
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects) {
+  return `Teaching ${todayClass}`;
+}
