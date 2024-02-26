0a938b969127b4e1f28a88faae1e7f332fd62f4f
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-26 17:42:58 +0200

    Fix eslint errors

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -6,8 +6,8 @@ const process = require('process');
  * prints in STDOUT the string argument
  * @param {String} string - string to be printed
  */
-function displayMessage (string) {
-  process.stdout.write(string + '\n');
+function displayMessage(string) {
+  process.stdout.write(`${string} \n`);
 }
 
 module.exports = displayMessage;
