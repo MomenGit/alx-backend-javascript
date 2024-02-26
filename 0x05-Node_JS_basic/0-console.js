0c799b4b2f7e6ca16ef6e5ff0338e32c8b2e1f4e
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-26 17:36:11 +0200

    Write 0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+
+/**
+ * prints in STDOUT the string argument
+ * @param {String} string - string to be printed
+ */
+function displayMessage(string) {
+  process.stdout.write(string + '\n');
+}
+
+module.exports = displayMessage;
