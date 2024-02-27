6ebe28829b59b9e34ff13785957fa4bc7ed700ab
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-27 13:42:07 +0200

    Fix eslint error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  let input = process.stdin.read();
+  const input = process.stdin.read();
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
