d29e61ef5168d3ea4c7e08599884003897aac8dd
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-27 23:11:11 +0200

    Fix output and use existsSync function

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -22,25 +22,28 @@ function processFieldStudents(students, field) {
 }
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const students = processStudentsData(data);
-    const fields = new Set(students.map((value) => value.field));
-    const studentsByField = [];
-    fields.forEach((field) => studentsByField.push({
-      fieldName: field,
-      fieldStudents: processFieldStudents(students, field),
-    }));
-    console.log(`Number of students: ${students.length}`);
-    studentsByField.forEach((value) => {
-      console.log(
-        `Number of students in ${value.fieldName}: ${value.fieldStudents.length}.`,
-        `List: ${value.fieldStudents.map((value) => value.firstName)}`,
-      );
-    });
-  } catch (err) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
+
+  const data = fs.readFileSync(path, 'utf8');
+  const students = processStudentsData(data);
+  const fields = new Set(students.map((value) => value.field));
+  const studentsByField = [];
+  fields.forEach((field) =>
+    studentsByField.push({
+      fieldName: field,
+      fieldStudents: processFieldStudents(students, field),
+    }),
+  );
+
+  console.log(`Number of students: ${students.length}`);
+  studentsByField.forEach((value) => {
+    console.log(
+      `Number of students in ${value.fieldName}: ${value.fieldStudents.length}.`,
+      `List: ${value.fieldStudents.map((value) => value.firstName).join(', ')}`,
+    );
+  });
 }
 
 module.exports = countStudents;
