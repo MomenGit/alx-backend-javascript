d7189fdef277e22a79217c8687fc478c1174114b
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-28 22:50:46 +0200

    Modify code

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,47 +1,50 @@
 #!/usr/bin/node
 const fs = require('fs');
 
+function processStudentsData(csv) {
+  const lines = csv.trim().split('\n');
+  lines.shift();
+  const students = lines
+    .filter((value) => value !== '')
+    .map((element) => {
+      const student = element.split(',');
+      return {
+        firstName: student[0],
+        lastName: student[1],
+        age: student[2],
+        field: student[3],
+      };
+    });
+  return students;
+}
+
+function processFieldStudents(students, field) {
+  return students.filter((student) => student.field === field);
+}
+
 function countStudents(path) {
-  if (!fs.existsSync(path)) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const students = processStudentsData(data);
+    const fields = new Set(students.map((value) => value.field));
+    const studentsByField = [];
+    fields.forEach((field) => studentsByField.push({
+      fieldName: field,
+      fieldStudents: processFieldStudents(students, field),
+    }));
+
+    console.log(`Number of students: ${students.length}`);
+    studentsByField.forEach((value) => {
+      const studentNames = value.fieldStudents
+        .map((value) => value.firstName)
+        .join(', ')
+        .trim();
+      const line = `Number of students in ${value.fieldName}: ${value.fieldStudents.length}. List: ${studentNames}`;
+      console.log(line);
+    });
+  } catch (err) {
     throw new Error('Cannot load the database');
   }
-  function processStudentsData(csv) {
-    const lines = csv.split('\n');
-    lines.shift();
-    const students = lines
-      .filter((value) => value !== '')
-      .map((element) => {
-        const student = element.split(',');
-        return {
-          firstName: student[0],
-          lastName: student[1],
-          age: student[2],
-          field: student[3],
-        };
-      });
-    return students;
-  }
-
-  function processFieldStudents(students, field) {
-    return students.filter((student) => student.field === field);
-  }
-
-  const data = fs.readFileSync(path, 'utf8');
-  const students = processStudentsData(data);
-  const fields = new Set(students.map((value) => value.field));
-  const studentsByField = [];
-  fields.forEach((field) => studentsByField.push({
-    fieldName: field,
-    fieldStudents: processFieldStudents(students, field),
-  }));
-
-  console.log(`Number of students: ${students.length}`);
-  studentsByField.forEach((value) => {
-    console.log(
-      `Number of students in ${value.fieldName}: ${value.fieldStudents.length}.`,
-      `List: ${value.fieldStudents.map((value) => value.firstName).join(', ')}`,
-    );
-  });
 }
 
 module.exports = countStudents;
