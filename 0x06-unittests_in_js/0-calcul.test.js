0e82a87453e4fe3153ae040b0a499692888fc928
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-01 17:23:16 +0200

    Write 0-calcul.test.js

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,26 @@
+const assert = require('chai').assert;
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('0 + 0 should return 0', () => assert.equal(calculateNumber(0, 0), 0));
+  it('0 + 0.5 should return 1', () => assert.equal(calculateNumber(0, 0.5), 1));
+  it('0.2 + 0.6 should return 1', () =>
+    assert.equal(calculateNumber(0.2, 0.6), 1));
+  it('1 + 3 should return 4', () => assert.equal(calculateNumber(1, 3), 4));
+  it('1 + 3.7 should return 5', () => assert.equal(calculateNumber(1, 3.7), 5));
+  it('1.2 + 3.7 should return 5', () =>
+    assert.equal(calculateNumber(1.2, 3.7), 5));
+  it('1.5 + 3.7 should return 6', () =>
+    assert.equal(calculateNumber(1.5, 3.7), 6));
+  it('-1 + 3 should return 2', () => assert.equal(calculateNumber(-1, 3), 2));
+  it('-1 + -3 should return -4', () =>
+    assert.equal(calculateNumber(-1, -3), -4));
+  it('-1 + -3.7 should return -5', () =>
+    assert.equal(calculateNumber(-1, -3.7), -5));
+  it('-1.2 + -3.7 should return -5', () =>
+    assert.equal(calculateNumber(-1.2, -3.7), -5));
+  it('-1.5 + -3.7 should return -6', () =>
+    assert.equal(calculateNumber(-1.5, -3.7), -5));
+  it('-1.6+ -3.7 should return -6', () =>
+    assert.equal(calculateNumber(-1.6, -3.7), -6));
+});
