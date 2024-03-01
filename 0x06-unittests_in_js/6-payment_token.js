db35c3cb82121212511ade8edffdaf1eb9239a53
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-02 00:34:27 +0200

    Write 6-payment_token.js

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {boolean} success
+ * @returns
+ */
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success)
+      resolve({
+        data: 'Successful response from the API',
+      });
+    else reject();
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
