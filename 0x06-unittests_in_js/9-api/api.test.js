3701abdf5e25e14cd34b88484874a49bddf2c6b0
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-03-02 17:05:23 +0200

    Write api.test.js

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const expect = require('chai').expect;
+const uri = 'http://localhost:7865';
+describe('Index page', () => {
+  it('Correct status code', (done) => {
+    request.get(uri, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('Correct status result', (done) => {
+    request.get(uri, (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+describe('/cart/:id route', () => {
+  it('Correct status code when id is number', (done) => {
+    request.get(`${uri}/cart/12`, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  it('Correct status code when is not a number', (done) => {
+    request.get(`${uri}/cart/hello`, (err, res, body) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
